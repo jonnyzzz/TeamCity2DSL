387b2a3d15218aa30398670e15ac8ed91a98b70d
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-01-10 01:31:41 +0100

    create first integration test: check TeamCity XML files -> DSL -> TeamCity XML Files transition introduces no changes to source files

--- src/test-integration-helper/src/main/kotlin/org/jonnyzzz/teamcity/dsl/util/XmlParsingTests.kt
+++ src/test-integration-helper/src/main/kotlin/org/jonnyzzz/teamcity/dsl/util/XmlParsingTests.kt
@@ -1,12 +1,5 @@
 package org.jonnyzzz.teamcity.dsl.util
 
-import org.jonnyzzz.teamcity.dsl.deleteAll
-import org.jonnyzzz.teamcity.dsl.div
-import org.jonnyzzz.teamcity.dsl.generating.DSLGenerating
-import org.jonnyzzz.teamcity.dsl.generating.DSLOptions
-import org.jonnyzzz.teamcity.dsl.having
-import org.jonnyzzz.teamcity.dsl.model.TCProject
-import org.jonnyzzz.teamcity.dsl.writeUTF
 import org.jonnyzzz.teamcity.dsl.xml.XmlGenerating
 import org.jonnyzzz.teamcity.dsl.xml.XmlParsing
 import org.junit.Assert
@@ -14,60 +7,33 @@ import org.junit.Test
 import java.io.File
 
 
-abstract class XmlParsingTestCase(val root: String) {
-
-  val testDataPath: File by lazy(LazyThreadSafetyMode.NONE) {
-    File("./testData").canonicalFile / root
+open class IntegrationTestBase() {
+  val XMLs_in by lazy {
+    File(System.getProperty("DATA_XMLS_IN")).apply {
+      Assert.assertTrue("$this should exist", this.isDirectory)
+    }
   }
 
-  protected fun loadProject(): List<TCProject> = XmlParsing.parse(testDataPath)
+  val XMLs_out by lazy {
+    File(System.getProperty("DATA_XMLS_OUT")).apply {
+      Assert.assertTrue("$this should exist", this.isDirectory)
+    }
+  }
 
   @Test
-  fun test_generate_kotlin_DSL() {
-
-    /// It is quite complicated to have Kotlin compile
-    /// call from the tests
-    ///
-    /// we use another approach:
-    ///  1) this tests generates Kotlin code to the DSL.Samples project
-    ///  2) marker files are included to simplify further tests
-    ///  3) Tests from DSL.Samples are used to ensure generated classes are back-mappable to XML
-
-    val orig = testDataPath
-    val model = XmlParsing.parse(orig)
-
-    val dest = File("./DSL.Samples/tests-gen").canonicalFile / orig.name.replace("[^a-zA-Z0-9]+".toRegex(), "_")
-    dest.deleteAll()
-    dest.mkdirs()
-
-    var options = having(DSLOptions()) {
-      packageName = "org.jonnyzzz.autodsl.tests.${orig.name.replace("[^a-zA-Z0-9]+".toRegex(), "_")}"
-    }
-
-    DSLGenerating.generate(model, dest, options)
-    dest.dumpFiles()
-
-    (dest / "gold.ref").writeUTF { testDataPath.absolutePath }
-    (dest / "gold.package").writeUTF { options.packageName }
+  fun test_assert_generated_xml_is_same() {
+    assertGeneratedTeamCityModel(XMLs_in, XMLs_out)
   }
 
   @Test
   fun test_load_safe_xml() {
-    val orig = testDataPath
-    val model = XmlParsing.parse(orig)
+    val model = XmlParsing.parse(XMLs_in)
 
     runUnderTempDirectory { temp ->
       XmlGenerating.generate(model, temp)
 
-      assertGeneratedTeamCityModel(orig, temp)
+      assertGeneratedTeamCityModel(XMLs_in, temp)
     }
-  }
-
-  @Test
-  fun projects_must_be_detected() {
-    val ps = loadProject().map { it.id }
-
-    Assert.assertTrue("Empty projects set!", ps.size > 0)
   }
 }
 
