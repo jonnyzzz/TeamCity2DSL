32e9fddaa13a03e0bc557dcb5667b5795642055c
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-02-08 20:29:44 +0100

    minimalistic tests and tasks (prototype for now)

--- src/gradle-plugin/src/main/kotlin/org/jonnyzzz/teamcity/dsl/gradle/plugin.kt
+++ src/gradle-plugin/src/main/kotlin/org/jonnyzzz/teamcity/dsl/gradle/plugin.kt
@@ -6,16 +6,27 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
 
 class GeneratorPlugin : Plugin<Project> {
-  override fun apply(target: Project?) {
+  override fun apply(project: Project?) {
+    if (project == null) return
 
+    project.tasks.create("xml2dsl", Xml2Dsl::class.java)
+    project.tasks.create("dsl2xml", Dsl2Xml::class.java)
   }
 }
 
-class DSLGeneratorTask : DefaultTask() {
+open class BaseDSLTask : DefaultTask() {
 
   @TaskAction
   fun `action!`() {
     println("Hohoho!")
   }
+}
+
+open class Xml2Dsl : BaseDSLTask() {
+
+}
+
+
+open class Dsl2Xml : BaseDSLTask() {
 
 }
---
+++ src/gradle-plugin/src/test/kotlin/org/jonnyzzz/teamcity/dsl/gradle/plugin-test.kt
@@ -0,0 +1,38 @@
+package org.jonnyzzz.teamcity.dsl.gradle
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class PluginTest {
+  lateinit var project : Project
+
+  @Before
+  fun setup() {
+    project = ProjectBuilder.builder().build()
+    project.apply {
+      it.plugin(GeneratorPlugin::class.java)
+    }
+  }
+
+  @Test
+  fun `apply plugin works`() {
+    //NOP
+  }
+
+  @Test
+  fun `exposes task dsl2xml`() {
+    Assert.assertTrue(
+      project.tasks.any { it.name == "dsl2xml"}
+    )
+  }
+
+  @Test
+  fun `exposes task xml2dsl`() {
+    Assert.assertTrue(
+            project.tasks.any { it.name == "xml2dsl" }
+    )
+  }
+}
