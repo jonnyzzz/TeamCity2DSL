d8ce63c46140a8d5d3ba6c6360f4454c8e847cda
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-02-16 01:05:29 +0100

    add plugin tests and stub implementation

--- src/gradle-plugin/src/main/kotlin/org/jonnyzzz/teamcity/dsl/gradle/plugin.kt
+++ src/gradle-plugin/src/main/kotlin/org/jonnyzzz/teamcity/dsl/gradle/plugin.kt
@@ -4,29 +4,52 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
+import org.jonnyzzz.teamcity.dsl.div
+import java.io.File
 
 class GeneratorPlugin : Plugin<Project> {
   override fun apply(project: Project?) {
     if (project == null) return
+
+/*
+    project.apply { config ->
+      config.plugin("java")
+      config.plugin("kotlin")
+    }
+*/
+
+    project.extensions.create("teamcity2dsl", DSLSettings::class.java, project.buildDir)
 
     project.tasks.create("xml2dsl", Xml2Dsl::class.java)
     project.tasks.create("dsl2xml", Dsl2Xml::class.java)
   }
 }
 
-open class BaseDSLTask : DefaultTask() {
+open class DSLSettings(private val baseDir : File) {
+  var `package` : String? = "org.jonnyzzz.teamcity.dsl.generated"
 
+  var dslPath : File? = baseDir / "dsl.generated"
+  var xmlPath : File? = baseDir / ".teamcity"
+}
+
+open class BaseDSLTask : DefaultTask() {
+  init {
+    group = "TeamCity2DSL"
+    outputs.upToDateWhen { false }
+  }
+}
+
+open class Xml2Dsl : BaseDSLTask() {
   @TaskAction
   fun `action!`() {
     println("Hohoho!")
   }
 }
 
-open class Xml2Dsl : BaseDSLTask() {
-
-}
-
 
 open class Dsl2Xml : BaseDSLTask() {
-
+  @TaskAction
+  fun `action!`() {
+    println("Hohoho!")
+  }
 }
