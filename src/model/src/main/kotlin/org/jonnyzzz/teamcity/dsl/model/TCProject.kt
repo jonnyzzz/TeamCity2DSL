a3c5b004c9a79092346629d350f96b49bbe029b1
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-03-12 22:37:35 +0100

    move

--- src/model/src/main/kotlin/org/jonnyzzz/teamcity/dsl/model/TCProject.kt
+++ src/model/src/main/kotlin/org/jonnyzzz/teamcity/dsl/model/TCProject.kt
@@ -28,22 +28,3 @@ abstract class TCProject(override val id : String) : TCUUID, TCProjectRef {
 
   var ordering by JXML / XSub(TCProjectOrdering::class.java)
 }
-
-class TCProjectOrdering {
-  private var rawProjectsOrder by JXML / "subprojectsOrder" / XAttribute("order")
-  private var rawBuildsOrder by JXML / "buildTypesOrder" / XAttribute("order")
-
-  var projectsOrder: List<String>?
-    get() = rawProjectsOrder?.split(",")
-    set(value) {
-      rawProjectsOrder = value?.joinToString(",")
-    }
-
-  var buildsOrder: List<String>?
-    get() = rawBuildsOrder?.split(",")
-    set(value) {
-      rawBuildsOrder = value?.joinToString(",")
-    }
-}
-
-fun TCProjectOrdering?.isEmpty() = this == null || (buildsOrder?.isEmpty() ?: true && projectsOrder?.isEmpty() ?: true)
