40d7e08cb51d18ae2c7ac402fa523c54845bf0fc
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-03-13 01:48:52 +0100

    rework integration tests to use Test task from Gradle to run all actions. #6 done

--- src/test-integration-helper/src/main/kotlin/org/jonnyzzz/teamcity/dsl/util/XmlParsingTests.kt
+++ src/test-integration-helper/src/main/kotlin/org/jonnyzzz/teamcity/dsl/util/XmlParsingTests.kt
@@ -1,39 +1,72 @@
 package org.jonnyzzz.teamcity.dsl.util
 
+import org.jonnyzzz.teamcity.dsl.main.teamcity_dls_generator_main
 import org.jonnyzzz.teamcity.dsl.xml.XmlGenerating
 import org.jonnyzzz.teamcity.dsl.xml.XmlParsing
 import org.junit.Assert
 import org.junit.Test
+import org.junit.experimental.categories.Category
 import java.io.File
 
+interface GenerateDSLCategory
+interface GenerateXMLCategory
+interface GenerateCheckCategory
 
-open class IntegrationTestBase() {
-  val XMLs_in by lazy {
-    File(System.getProperty("DATA_XMLS_IN")).apply {
-      Assert.assertTrue("$this should exist", this.isDirectory)
-    }
+
+internal object ProvidedPaths {
+  val DSL_package = "org.jonnyzzz.autodsl.integration.generated"
+  val DSL_classes by lazyPropertyPath("DATA_DSL_CLASSES")
+  val DSL_out by lazyPropertyPath("DATA_DSL_OUT")
+
+  val XMLs_in by lazyPropertyPath("DATA_XMLS_IN")
+  val XMLs_out by lazyPropertyPath("DATA_XMLS_OUT")
+
+  private fun lazyPropertyPath(property : String) = lazy {
+    val path = System.getProperty(property)
+    Assert.assertNotNull("property $property is not defined", path)
+    val file = File(path)
+    return@lazy file.absoluteFile
   }
+}
 
-  val XMLs_out by lazy {
-    File(System.getProperty("DATA_XMLS_OUT")).apply {
-      Assert.assertTrue("$this should exist", this.isDirectory)
-    }
-  }
-
+@Category(GenerateDSLCategory::class)
+abstract class GenerateDSLsTestBase {
   @Test
-  fun test_assert_generated_xml_is_same() {
-    assertGeneratedTeamCityModel(XMLs_in, XMLs_out)
-  }
-
-  @Test
-  fun test_load_safe_xml() {
-    val model = XmlParsing.parse(XMLs_in)
+  fun `load save XML is ID`() {
+    val model = XmlParsing.parse(ProvidedPaths.XMLs_in)
 
     runUnderTempDirectory { temp ->
       XmlGenerating.generate(model, temp)
 
-      assertGeneratedTeamCityModel(XMLs_in, temp)
+      assertGeneratedTeamCityModel(ProvidedPaths.XMLs_in, temp)
     }
+  }
+
+  @Test
+  fun `generate DSL from XML`() {
+    teamcity_dls_generator_main("import",
+            ProvidedPaths.DSL_package,
+            ProvidedPaths.XMLs_in.absolutePath,
+            ProvidedPaths.DSL_out.absolutePath)
+  }
+}
+
+@Category(GenerateXMLCategory::class)
+abstract class GenerateXMLsTestBase {
+  @Test
+  fun `generate XML from DSL`() {
+    teamcity_dls_generator_main("generate",
+            ProvidedPaths.DSL_package,
+            ProvidedPaths.XMLs_out.absolutePath,
+            ProvidedPaths.DSL_classes.absolutePath)
+  }
+}
+
+@Category(GenerateCheckCategory::class)
+abstract class IntegrationTestBase() {
+  @Test
+  fun `check generated XML is same as test data`() {
+    assertGeneratedTeamCityModel(ProvidedPaths.XMLs_in, ProvidedPaths.XMLs_out)
   }
 }
 
