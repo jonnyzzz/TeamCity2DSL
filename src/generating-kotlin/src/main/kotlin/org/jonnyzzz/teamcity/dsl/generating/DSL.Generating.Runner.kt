5350f44dfcf1c88df604d376278fca0372529635
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-03-20 23:21:26 +0100

    drop clustering for runners

--- src/generating-kotlin/src/main/kotlin/org/jonnyzzz/teamcity/dsl/generating/DSL.Generating.Runner.kt
+++ src/generating-kotlin/src/main/kotlin/org/jonnyzzz/teamcity/dsl/generating/DSL.Generating.Runner.kt
@@ -9,29 +9,14 @@ import java.util.*
 import kotlin.collections.linkedMapOf
 
 fun KotlinWriter.generateRunners(runners: List<TCSettingsRunner>?): KotlinWriter.(TCSettingsRunner) -> Unit {
-  val that = this
-  with(object : DSLClusteringGenerator<TCSettingsRunner>() {
-    override fun nameDMixin(d: TCSettingsRunner): String = ::runnerMixin.name
-    override fun funDMixin(d: TCSettingsRunner): String = ::runnerMixin.name + "()"
-    override fun funD(d: TCSettingsRunner): String = "${TCBuildSettings::runner.name}(${d.id?.quote()}, ${d.runnerType?.quote()})"
 
-    override fun predefinedMixins(): LinkedHashMap<String, TCSettingsRunner.() -> Unit> = linkedMapOf(
-            ::normalStep.name to normalStep.asBuilder(),
-            ::alwaysStep.name to alwaysStep.asBuilder(),
-            ::coverageEMMA.name to coverageEMMA.asBuilder(),
-            ::coverageIDEA.name to coverageIDEA.asBuilder(),
-            ::coverageJOCOCO.name to coverageJOCOCO.asBuilder()
-    )
+  return { d ->
+    val item = d
 
-    override fun detectMixins(ds: List<TCSettingsRunner>): List<TCSettingsRunner.() -> Unit> = clusteringRunner(ds)
-    override fun newD(): TCSettingsRunner = TCSettingsRunner()
-
-    override fun KotlinWriter.generateImplementationBlock(item: TCSettingsRunner, baseItem: TCSettingsRunner) {
+    block("${TCBuildSettings::runner.name}(${d.id?.quote()}, ${d.runnerType?.quote()})") {
       if (item.name != null) setter("name", item.name)
 
-      params(item.parameters, baseItem.parameters)
+      params(item.parameters)
     }
-  }) {
-    return that.generate(runners)
   }
 }
