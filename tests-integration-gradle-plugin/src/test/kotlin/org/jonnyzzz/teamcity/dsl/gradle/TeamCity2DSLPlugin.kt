ef240383a69cc204f11d2e0ba280b090565b5c18
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-06-11 01:25:27 +0200

    pass version as a dedicated parameter

--- tests-integration-gradle-plugin/src/test/kotlin/org/jonnyzzz/teamcity/dsl/gradle/TeamCity2DSLPlugin.kt
+++ tests-integration-gradle-plugin/src/test/kotlin/org/jonnyzzz/teamcity/dsl/gradle/TeamCity2DSLPlugin.kt
@@ -6,6 +6,7 @@ import javax.xml.parsers.DocumentBuilderFactory
 
 object TeamCity2DSLPlugin {
   val DSL_PLUGIN_CLASSPATH by lazy { System.getProperty("TEST_PLUGIN_CLASSPATH")!! }
+  val DSL_PLUGIN_VERSION by lazy { System.getProperty("TEST_PLUGIN_VERSION")!! }
   val DSL_PLUGIN_NAME by lazy { System.getProperty("TEST_PLUGIN_NAME")!! }
 
   val DSL_PLUGIN_LATEST_PUBLIC_VERSION by lazy {
@@ -39,7 +40,7 @@ object TeamCity2DSLPlugin {
       }
 
       dependencies {
-        classpath '$DSL_PLUGIN_CLASSPATH'
+        classpath '$DSL_PLUGIN_CLASSPATH:$DSL_PLUGIN_VERSION'
       }
     }
 
