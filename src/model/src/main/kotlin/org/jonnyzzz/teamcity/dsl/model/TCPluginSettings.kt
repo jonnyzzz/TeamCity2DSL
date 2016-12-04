f7c382e64c87576b26b8c8593e94e8ab619d205e
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-12-04 10:28:01 -0800

    implement project-extensions DSL (basic one for now)

--- src/model/src/main/kotlin/org/jonnyzzz/teamcity/dsl/model/TCPluginSettings.kt
+++ src/model/src/main/kotlin/org/jonnyzzz/teamcity/dsl/model/TCPluginSettings.kt
@@ -1,7 +1,6 @@
 package org.jonnyzzz.teamcity.dsl.model
 
-import org.jonnyzzz.kotlin.xml.bind.XAnyElements
-import org.jonnyzzz.kotlin.xml.bind.XRoot
+import org.jonnyzzz.kotlin.xml.bind.*
 import org.jonnyzzz.kotlin.xml.bind.jdom.JXML
 import org.jonnyzzz.kotlin.xml.bind.jdom.XUnknown
 
@@ -9,4 +8,17 @@ import org.jonnyzzz.kotlin.xml.bind.jdom.XUnknown
 @XRoot("settings")
 class TCPluginSettings {
   var settings by JXML / XAnyElements / XUnknown
+}
+
+@XRoot("project-extensions")
+class TCProjectExtensions {
+  var extensions by JXML / XElements("extension") / XSub(TCProjectExtension::class.java)
+}
+
+@XRoot("extension")
+class TCProjectExtension {
+  var id by JXML / XAttribute("id")
+  var type by JXML / XAttribute("type")
+
+  var parameters by JXML / "parameters" / XElements("param") / XSub(TCParameter::class.java) - listOf()
 }
